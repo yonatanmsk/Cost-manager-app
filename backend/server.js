ff77231dd6c2016383eb90326c102307b2a15cef
yonatanmsk <yonatanmsk@gmail.com>
2022-06-25 13:21:59 +0300

    final update finally

--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,25 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+// creates a reference to the environment variables
 require('dotenv').config();
 
+// creates an app instance and the server port
 const app = express();
 const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
 
 app.use(cors());
 app.use(express.json());
-
-const uri = process.env.ATLAS_URI;
 mongoose.connect(uri);
 
+// creates the connection to mongoDB
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
+// creates the variables which define the routes - REStful api
 const costsRouter = require('./routes/costs');
 const usersRouter = require('./routes/users');
 const computesRouter = require('./routes/computes');
